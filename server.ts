fc97bb68a637f8191bf0ac55ed6d409797b52134
Njahi98 <njahioussama75@gmail.com>
2025-05-25 22:54:27 +0100

    - Updated package.json and package-lock.json to include new dependencies: jsonwebtoken, multer, passport, passport-jwt, and zod - Created server.ts with Express setup, including middleware for CORS and JSON parsing

---
+++ server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { PrismaClient } from "./generated/prisma";
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
